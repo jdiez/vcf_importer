306a58e71464332ec7d4688a231425c109970892
andrew carroll <acarroll@dnanexus.com>
2012-07-17 16:52:48 -0700

    Add support for streaming compressed input into the importer

--- parseVcf.py
+++ parseVcf.py
@@ -7,7 +7,12 @@ import re
 import dxpy
 import subprocess
 import time
+import logging
 
+sys.path.append('/usr/local/lib/')
+import magic
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 
@@ -19,8 +24,8 @@ def main():
 
     inputFile = dxpy.download_dxfile(job['input']['vcf'], 'output.vcf')
     
-    header = extractHeader(open('output.vcf', 'r')).strip()
-  
+    header = extractHeader('output.vcf').strip()
+    
     variants_schema = [
       {"name": "chr", "type": "string"}, 
       {"name": "lo", "type": "int32"},
@@ -54,6 +59,7 @@ def main():
 
     if job['input']['store_samples_individually']:        
         tabSplit = header.strip().split("\t")
+        print tabSplit
         for x in tabSplit[9:]:
             table = dxpy.new_dxgtable(variants_schema, indices=[dxpy.DXGTable.genomic_range_index("chr","lo","hi", 'gri')])
             details = {'sample':x, 'original_contigset':job['input']['reference'], 'original_file':job['input']['vcf']}
@@ -114,13 +120,69 @@ def setTableDetails(table, details):
     simpleVar.set_details(details)
     
 
-def extractHeader(vcfFile):
+def extractHeader(vcfFileName):
     header = ''
-    fileIter = vcfFile.__iter__()
-    while 1:
-        try:
-            input = fileIter.next()
-            if(input[1] != "#"):
-                return input
-        except:
-            break
+    with unpack_and_open(vcfFileName) as vcfFile:
+        while 1:
+            line = vcfFile.next()
+            if line == '':
+                break
+            if line[1] != "#":
+                return line
+            
+def unpack_and_open(input):
+    m = magic.Magic()
+
+    # determine compression format
+    try:
+        file_type = m.from_file(input)
+    except:
+        raise dxpy.ProgramError("Unable to identify compression format")
+
+    print file_type
+
+    # if uncompressed open the file and return a handle to it
+    try:
+        if file_type == 'ASCII text' or file_type == 'ASCII English text, with very long lines':
+            return open(input)
+    except:
+        raise dxpy.ProgramError("Detected uncompressed input but unable to open file. File may be corrupted.")
+
+    # if we find a tar file throw a program error telling the user to unpack it
+    if file_type == 'application/x-tar':
+        raise dxpy.ProgramError("Program does not support tar files.  Please unpack.")
+
+    # since we haven't returned, the file is compressed.  Determine what program to use to uncompress
+    uncomp_util = None
+    if file_type == 'XZ compressed data':
+        uncomp_util = 'xzcat'
+    elif file_type[:21] == 'bzip2 compressed data':
+        uncomp_util = 'bzcat'
+    elif file_type[:20] == 'gzip compressed data':
+        uncomp_util = 'zcat'
+    elif file_type == 'POSIX tar archive (GNU)' or 'tar' in file_type:
+        raise dxpy.ProgramError("Found a tar archive.  Please untar your sequences before importing")
+    else:
+        raise dxpy.ProgramError("Unsupported compression type.  Supported formats are xz, gzip, bzip, and uncompressed")
+
+    # with that in hand, open file for reading.  If we find a tar archive then exit with error.
+    try:
+        with subprocess.Popen([uncomp_util, input], stdout=subprocess.PIPE).stdout as pipe:
+            line = pipe.next()
+        uncomp_type = m.from_buffer(line)
+        print uncomp_type
+    except:
+        raise dxpy.ProgramError("Error detecting file format after decompression")
+
+    if uncomp_type == 'POSIX tar archive (GNU)' or 'tar' in uncomp_type:
+        raise dxpy.ProgramError("Found a tar archive after decompression.  Please untar your sequences before importing")
+    elif uncomp_type != 'ASCII text':
+        raise dxpy.ProgramError("After decompression found file type other than plain text")
+    
+    try:
+        return subprocess.Popen([uncomp_util, input], stdout=subprocess.PIPE).stdout
+    except:
+        raise dxpy.ProgramError("Unable to open compressed input for reading")
+            
+
+            
